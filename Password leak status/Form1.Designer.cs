917e091b51d3acaa0873feafa45c225bb4836311
junyoung kim <38175394+rlawnsdud117@users.noreply.github.com>
2020-07-16 15:04:05 +0900

    Password-leak-status
    juni65423@gmail.com

---
+++ Password leak status/Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace Password_leak_status
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(119, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "패스워드(비밀번호) :";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(134, 20);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.PasswordChar = '*';
+            this.textBox1.Size = new System.Drawing.Size(273, 21);
+            this.textBox1.TabIndex = 1;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(414, 22);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(100, 16);
+            this.checkBox1.TabIndex = 2;
+            this.checkBox1.Text = "비밀번호 표시";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            this.checkBox1.CheckStateChanged += new System.EventHandler(this.checkBox1_CheckStateChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(107, 47);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(337, 33);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "확인";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(519, 91);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "앗! 잠깐만 내 패스워드가 안전하니?";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Password leak status/Form1.cs
@@ -0,0 +1,96 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Security.Cryptography;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Password_leak_status
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked == true)
+            {
+                textBox1.PasswordChar = default(char);
+            }
+            else
+            {
+                textBox1.PasswordChar = '*';
+            }
+        }
+
+        private void checkBox1_CheckStateChanged(object sender, EventArgs e)
+        {
+
+        }
+        static string Hash(string input)
+        {
+            var hash = new SHA1Managed().ComputeHash(Encoding.UTF8.GetBytes(input));
+            return string.Concat(hash.Select(b => b.ToString("x2")));
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "")
+            {
+                MessageBox.Show("비밀번호를 입력해주세요.", "경고", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+            string sha1 = Hash(textBox1.Text);
+            string sha_pper = sha1.ToUpper();
+            string Str_SHA1 = sha_pper.Substring(0, 8);
+            HttpWebRequest myReq = (HttpWebRequest)WebRequest.Create("https://exts.kr/api.hackedpw.json?prefix=" + Str_SHA1 + "");
+            //HttpWebResponse 객체 받아옴
+            HttpWebResponse wRes = (HttpWebResponse)myReq.GetResponse();
+            // Response의 결과를 스트림을 생성합니다.
+            Stream respGetStream = wRes.GetResponseStream();
+            StreamReader readerGet = new StreamReader(respGetStream, Encoding.UTF8);
+            string resultGet = readerGet.ReadToEnd();
+            var jo = JObject.Parse(resultGet);
+
+            /*
+            {
+  "success": true,
+  "data": {
+    "hacked": true,
+    "count": 1296186
+  }
+}
+             */
+            var data = (JObject)jo["data"];
+            foreach (var Str_data in data)
+            {
+                string Strdata = Str_data.Key;
+
+                string adata = jo["data"]["hacked"].ToString();
+
+                if(adata == "True")
+                {
+                    string count = jo["data"]["count"].ToString();
+                    MessageBox.Show("당신의 패스워드가 유출 되었습니다. \r\n패스워드 유출횟수: " + count + " 번 입니다.", "경고", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    return;
+                }
+                else
+                {
+                    MessageBox.Show("당신의 패스워드 유출되지 않았습니다.", "유출되지 않음", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    return;
+                }
+
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }        
+}
